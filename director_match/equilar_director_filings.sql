5a332d4c8228a7d0f056bdbf1040498303ab2787
Ian Gow <iandgow@gmail.com>
2015-02-12 12:44:14 -0500

    Tweaked code to provide a full URL for links to filings.

--- director_match/equilar_director_filings.sql
+++ director_match/equilar_director_filings.sql
@@ -2,7 +2,7 @@ WITH directors AS (
     SELECT director.equilar_id(director_id) AS equilar_id, fy_end, director
     FROM director.director)
 SELECT *,
-    regexp_replace(file_name, 
+    'http://www.sec.gov/Archives/' || regexp_replace(file_name, 
                    E'(\\d{10})-(\\d{2})-(\\d{6})\\.txt', 
                    E'\\1\\2\\3') AS url
 FROM directors 
