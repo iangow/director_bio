24cc5361bed6bb529647feeb999beabb1489d536
Ian Gow <igow@hbs.edu>
2015-06-23 11:09:17 -0400

    Added query to identify problematic director matches.
    - This probably belongs in acct_data

---
+++ director_match/problematic_name_matches.sql
@@ -0,0 +1,25 @@
+﻿SET work_mem='10GB';
+
+WITH 
+
+directors AS (
+    SELECT DISTINCT director.equilar_id(director_id), 
+          (director.equilar_id(director_id),
+           director.director_id(director_id))::equilar_director_id AS director_id,
+          (director.parse_name(director)).*, director, company, fy_end, age, gender
+    FROM director.director),
+
+filings AS (
+    SELECT DISTINCT equilar_id, fy_end, file_name
+    FROM director.equilar_proxies
+    WHERE file_name IS NOT NULL)
+    
+SELECT director, file_name, 
+    array_agg(age) AS ages,
+    array_agg(director_id) AS director_ids,
+    count(director_id) AS num_directors
+FROM directors
+INNER JOIN filings
+USING (equilar_id, fy_end)
+GROUP BY director, file_name
+HAVING count(DISTINCT director_id) > 1;
