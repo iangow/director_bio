65dc78f504a4f20d7b10c38cc589670ce5610487
Ian Gow <igow@hbs.edu>
2015-06-23 11:14:01 -0400

    Fixed backup script.

--- pg_backup.sh
+++ pg_backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 pg_dump --host iangow.me --format custom --no-tablespaces -O --verbose --table "public.mirror_filing" "crsp" --file ~/Dropbox/pg_private/mirror_filings.backup
-pg_dump --host iangow.me --format custom --no-tablespaces -O --verbose --table "public.mirror_filing" "crsp" --file ~/Dropbox/pg_private/mirror_biography.backup
+pg_dump --host iangow.me --format custom --no-tablespaces -O --verbose --table "public.mirror_biography" "crsp" --file ~/Dropbox/pg_private/mirror_biography.backup
 pg_dump --host iangow.me --format custom --no-tablespaces -O --verbose --schema "public" "crsp" --file ~/Dropbox/pg_private/public.backup
 
