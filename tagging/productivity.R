3fbf20674c3e7260a2d15cee04a62c870f502e33
Ian Gow <igow@hbs.edu>
2015-07-30 15:16:38 -0400

    Moved RA productivity analysis to a separate file.

---
+++ tagging/productivity.R
@@ -0,0 +1,38 @@
+
+# Look at statistics on filings ----
+bio_data$username <- gsub("^(.*?)@(.*)$", "\\1",
+                          bio_data$username,
+                          perl=TRUE)
+table(bio_data$username)
+library(dplyr)
+bio_data %>%
+   group_by(username) %>%
+   summarise(num_filings = n_distinct(uri))
+
+# Produce some plots ----
+if (!dir.exists("figures")) dir.create("figures")
+pdf(file="figures/productivity.pdf", paper = "USr", width=9)
+
+library(ggplot2)
+library(scales) 
+bio_data %>%
+    group_by(username, uri) %>%
+    summarise(start_time = min(updated), end_time = max(updated), 
+              time_taken=end_time-start_time) -> 
+    plot_data
+
+plot_data %>% 
+    ggplot(aes(x=end_time, fill=username)) + 
+        geom_histogram(binwidth=60*60) + 
+        scale_x_datetime(name="Time (hour)", 
+                         breaks=("2 hour"), minor_breaks=("1 hour"),
+                          labels=date_format("%H")) +
+        ggtitle("Filings per hour") 
+       
+
+plot_data %>%
+    filter(time_taken<600) %>%
+    ggplot(aes(x=time_taken, fill=username)) +
+        geom_histogram( binwidth=20) +
+        ggtitle("Elapsed time for each filing")
+dev.off()
