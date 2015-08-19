147a257685ad72878a6a6c77d56f8781200ed19b
Ian Gow <igow@hbs.edu>
2015-08-19 12:02:30 -0400

    Tweaks to database set-up code.

--- db_setup.R
+++ db_setup.R
@@ -4,7 +4,7 @@ pg <- dbConnect(PostgreSQL())
 
 rs <- dbGetQuery(pg, "
     CREATE SCHEMA IF NOT EXISTS director_bio AUTHORIZATION igow;         
-    CREATE ROLE director_bio_team;
+    -- CREATE ROLE  director_bio_team;
 
     GRANT USAGE ON SCHEMA director_bio TO director_bio_team;
     GRANT CREATE ON SCHEMA director_bio TO director_bio_team;
@@ -13,3 +13,5 @@ rs <- dbGetQuery(pg, "
     GRANT director_bio_team TO igow;
                  
     GRANT SELECT ON targeted.equilar_boardex TO director_bio_team")
+
+rs <- dbDisconnect(pg)
