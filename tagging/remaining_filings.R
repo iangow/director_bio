e200d56facaf89476c51cb34f33fcc9e9116d853
Ian Gow <igow@hbs.edu>
2015-08-20 13:07:04 -0400

    Some initial analysis of "remaining filings".

--- tagging/remaining_filings.R
+++ tagging/remaining_filings.R
@@ -11,3 +11,33 @@ remaining_filings <- gs_read(gs, ws = "filing_list")
 table(is.na(remaining_filings$assigned_ra))
 table(remaining_filings$assigned_ra, useNA="ifany")
 with(remaining_filings, table(is.na(issue_category), assigned_ra))
+
+remaining_filings$file_name <-
+    gsub("^.*/highlight/(\\d+)/(\\d{10})(\\d{2})(\\d{6}).*$", 
+         "edgar/data/\\1/\\2-\\3-\\4.txt", remaining_filings$url)
+
+library(RPostgreSQL)
+pg <- dbConnect(PostgreSQL())
+
+rs <- dbWriteTable(pg, c("director_bio", "remaining_filings"), 
+                   as.data.frame(remaining_filings),
+             overwrite=TRUE, row.names=FALSE)
+
+sql <- "ALTER TABLE director_bio.remaining_filings OWNER TO director_bio_team;"
+rs <- dbGetQuery(pg, sql)
+
+sql <- "CREATE INDEX ON director_bio.remaining_filings (file_name)"
+rs <- dbGetQuery(pg, sql)
+
+rs <- dbDisconnect(pg)
+
+pg <- dbConnect(PostgreSQL())
+rem_filings <- dbGetQuery(pg,"
+    SELECT DISTINCT file_name, username, assigned_ra, c.issue_category
+    FROM director_bio.bio_data
+    RIGHT JOIN director_bio.remaining_filings
+    USING (file_name)
+    LEFT JOIN director_bio.tagging_issues AS c
+    USING (file_name)")
+rs <- dbDisconnect(pg)
+
