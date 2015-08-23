a1b2a7a32ba59befc746f68e3226493772975851
Ian Gow <igow@hbs.edu>
2015-08-23 15:03:40 -0400

    Added code to identify strikethrough cases for re-tagging.

--- tagging/strikethrough.R
+++ tagging/strikethrough.R
@@ -13,6 +13,12 @@ clean_bio <- function(bio) {
     new_text
 }
 
+# Flag cases without bios for re-tagging
+re_tag <- unique(subset(strikethrough, is.na(clean_bio), select=url))
+write.csv(re_tag, "~/Google Drive/director_bio/bio_tagging/re_tag.csv", 
+          row.names = FALSE)
+# I manually added these bios to the "remaining filing" sheet with a note 
+# to re-tag them.
 
 strikethrough$clean_bio <- 
     unlist(lapply(strikethrough$bio, clean_bio))
