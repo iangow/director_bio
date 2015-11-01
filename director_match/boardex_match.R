9c6bd497ee94c7cfb2d265fd6cff3cbc85578c43
Ian Gow <igow@hbs.edu>
2015-11-01 18:38:23 -0500

    Fixes to Equilar ID to BoardEx match.
    - Switched to PERMCO from PERMNO.
    - Fixed double-counting in summary statistics.

--- director_match/boardex_match.R
+++ director_match/boardex_match.R
@@ -1,23 +1,23 @@
 library(dplyr)
 pg <- src_postgres()
 
-# Use CRSP to match tickers and CUSIPs to PERMNOs ----
+# Use CRSP to match tickers and CUSIPs to permcos ----
 stocknames <- tbl(pg, sql("
-    SELECT permno, comnam, namedt, nameenddt, ncusip AS cusip, ticker
+    SELECT permco, comnam, namedt, nameenddt, ncusip AS cusip, ticker
     FROM crsp.stocknames"))
 
 crsp_cusips <-
     stocknames %>%
-    select(permno, cusip) %>%
+    select(permco, cusip) %>%
     distinct()
 
 crsp_tickers <-
     stocknames %>%
-    select(permno, ticker, namedt, nameenddt) %>%
-    group_by(permno, ticker) %>%
+    select(permco, ticker, namedt, nameenddt) %>%
+    group_by(permco, ticker) %>%
     summarize(start_date=min(namedt), end_date=max(nameenddt))
 
-# Match Equilar to CRSP PERMNOs where possible ----
+# Match Equilar to CRSP permcos where possible ----
 director_ciks <- tbl(pg, sql("
     SELECT *
     FROM director.ciks")) %>%
@@ -34,7 +34,7 @@ co_fin <- tbl(pg, sql("
 director_cusip <-
     co_fin %>%
     inner_join(crsp_cusips) %>%
-    select(equilar_id, cusip, permno) %>%
+    select(equilar_id, cusip, permco) %>%
     distinct() %>%
     mutate(match_type=sql("'cusip'::text"))
 
@@ -43,19 +43,19 @@ director_ticker <-
     anti_join(director_cusip) %>%
     left_join(crsp_tickers) %>%
     filter((fy_end >= start_date & fy_end <= end_date) | is.na(start_date)) %>%
-    select(equilar_id, cusip, permno) %>%
+    select(equilar_id, cusip, permco) %>%
     distinct() %>%
     mutate(match_type=sql("'ticker'::text"))
 
-director_permno <-
+director_permco <-
     director_cusip %>%
     union(director_ticker) %>%
     collect()
 
-director_permno$match_type[is.na(director_permno$permno)] <- "none"
-table(director_permno$match_type)
+director_permco$match_type[is.na(director_permco$permco)] <- "none"
+table(director_permco$match_type)
 
-# Match BoardEx to CRSP PERMNOs where possible ----
+# Match BoardEx to CRSP permcos where possible ----
 
 # BoardEx companies
 co_profile <- tbl(pg, sql("
@@ -67,7 +67,7 @@ co_profile <- tbl(pg, sql("
 co_profile_cusip <-
     co_profile %>%
     inner_join(crsp_cusips) %>%
-    select(boardid, cusip, permno) %>%
+    select(boardid, cusip, permco) %>%
     distinct() %>%
     mutate(match_type=sql("'cusip'::text"))
 
@@ -76,44 +76,50 @@ co_profile_ticker <-
     anti_join(co_profile_cusip) %>%
     left_join(crsp_tickers) %>%
     # filter(fy_end >= start_date, fy_end <= end_date) %>%
-    select(boardid, cusip, permno) %>%
+    select(boardid, cusip, permco) %>%
     distinct() %>%
     mutate(match_type=sql("'ticker'::text"))
 
-co_profile_permno <-
+co_profile_permco <-
     co_profile_cusip %>%
     union(co_profile_ticker) %>%
     distinct() %>%
     collect()
 
-co_profile_permno$match_type[is.na(co_profile_permno$permno)] <- "none"
-table(co_profile_permno$match_type)
+co_profile_permco$match_type[is.na(co_profile_permco$permco)] <- "none"
+table(co_profile_permco$match_type)
 
-# Combine Equilar and BoardEx using CUSIPs and PERMNOs ----
+# Combine Equilar and BoardEx using CUSIPs and permcos ----
 boardex_merge_cusip <-
-    director_permno %>%
+    director_permco %>%
     select(equilar_id, cusip) %>%
-    left_join(co_profile_permno %>% select(boardid, cusip),
+    inner_join(co_profile_permco %>% select(boardid, cusip),
               by="cusip") %>%
     select(equilar_id, boardid) %>%
     distinct() %>%
     mutate(match_type="cusip")
 
-boardex_merge_permno <-
-    director_permno %>%
-    anti_join(boardex_merge_cusip) %>%
-    select(equilar_id, permno) %>%
-    filter(!is.na(permno)) %>%
-    left_join(co_profile_permno %>%
-                  select(boardid, permno)) %>%
+boardex_merge_permco <-
+    director_permco %>%
+    select(equilar_id, permco) %>%
+    anti_join(boardex_merge_cusip %>% filter(!is.na(boardid))) %>%
+    left_join(co_profile_permco %>%
+                  select(boardid, permco) %>%
+                  filter(!is.na(permco))) %>%
     select(equilar_id, boardid) %>%
     distinct() %>%
-    mutate(match_type="permno")
+    mutate(match_type="permco")
 
 boardex_merge <-
     boardex_merge_cusip %>%
-    union(boardex_merge_permno) %>%
+    union(boardex_merge_permco) %>%
+    arrange(equilar_id) %>%
     collect()
 
 boardex_merge$match_type[is.na(boardex_merge$boardid)] <- "none"
-table(boardex_merge$match_type)
+
+boardex_merge %>%
+    select(equilar_id, match_type) %>%
+    distinct() %>%
+    group_by(match_type) %>%
+    summarize(count=n())
