ce3eb6efbfd4c2fb8ba8d4e5715559cc83a24006
Ian Gow <igow@hbs.edu>
2015-11-05 11:32:14 -0500

    Added flag for multiple PERMNO matches in directorship_results.

--- directorships/create_directorship_results.sql
+++ directorships/create_directorship_results.sql
@@ -3,14 +3,26 @@ SET work_mem='2GB';
 DROP TABLE IF EXISTS director_bio.directorship_results;
 
 CREATE TABLE director_bio.directorship_results AS
-SELECT b.*, c.result, c.non_match, d.date_filed
-FROM director_bio.bio_data AS a
-INNER JOIN director_bio.other_directorships AS b
-USING (director_id, fy_end)
-INNER JOIN director_bio.regex_results AS c
-USING (file_name, director_id, other_director_id)
-INNER JOIN filings.filings AS d
-USING (file_name);
+WITH raw_data AS (
+    SELECT b.*, a.director, c.result, c.non_match, d.date_filed
+    FROM director_bio.bio_data AS a
+    INNER JOIN director_bio.other_directorships AS b
+    USING (director_id, fy_end)
+    INNER JOIN director_bio.regex_results AS c
+    USING (file_name, director_id, other_director_id)
+    INNER JOIN filings.filings AS d
+    USING (file_name)),
+
+mult_permnos AS (
+    SELECT director_id, fy_end, other_director_id,
+        count(DISTINCT other_permno) > 1 AS mult_permnos
+    FROM raw_data
+    GROUP BY director_id, fy_end, other_director_id)
+
+SELECT *
+FROM raw_data
+INNER JOIN mult_permnos
+USING (director_id, fy_end, other_director_id);
 
 ALTER TABLE director_bio.directorship_results OWNER TO director_bio_team;
 CREATE INDEX ON director_bio.directorship_results (director_id);
