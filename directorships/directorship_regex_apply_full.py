eaff69d3f7208373c26f6bdf687a44550324f4e5
Ian Gow <iandgow@gmail.com>
2015-11-14 09:57:05 -0500

    Tweaks to code applying regexes.
    - Code clean-up.
    - Added comments.
    - Added code to pickle data.

--- directorships/directorship_regex_apply_full.py
+++ directorships/directorship_regex_apply_full.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from directorship_regex import names_in_bio
-import sqlalchemy as sa
 import pandas as pd
 
+# Get data from database
+import sqlalchemy as sa
 import psycopg2 as pg
 from pandas.io.sql import read_sql
 
@@ -18,14 +18,18 @@ sql = """
     USING (director_id, fy_end)
     LEFT JOIN director_bio.tagged_names
     USING (other_equilar_id)
-    -- WHERE director_id=(25387,913921)
-"""
+    """
 
 df = pd.read_sql(sa.text(sql), engine)
 
-df['result'] =  df.apply(lambda row: names_in_bio(row['bio'],
-                                                  row['other_names']),
-                            axis=1)
+# df.to_pickle('directorships.pickle')
+# df = pd.read_pickle('directorships.pickle')
+
+# Apply regular expression code to bios and names
+from directorship_regex import names_in_bio
+df['result'] = df.apply(lambda row: names_in_bio(row['bio'],
+                                                 row['other_names']),
+                        axis=1)
 df['non_match'] = df['result'].map(lambda x: not x)
 
 # Delete columns not needed any more
@@ -36,9 +40,12 @@ del df['other_names']
 df.to_sql('regex_results', engine, schema="director_bio",
          if_exists="replace", index=False)
 
-engine.execute("ALTER TABLE director_bio.regex_results OWNER TO director_bio_team")
-
-engine.execute("""
+# Do some database-related clean-up
+engine.execute(
+    """
+    ALTER TABLE director_bio.regex_results
+    OWNER TO director_bio_team;
+    
     ALTER TABLE director_bio.regex_results
         ALTER COLUMN director_id TYPE equilar_director_id
         USING director_id::equilar_director_id;
@@ -48,4 +55,5 @@ engine.execute("""
         USING other_director_id::equilar_director_id;
 
     CREATE INDEX ON director_bio.regex_results
-        (file_name, director_id, other_director_id)""")
+        (file_name, director_id, other_director_id)
+    """)
