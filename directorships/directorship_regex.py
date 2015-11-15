9725d40b0e96b70d9918168420c39229e80bd2f9
Ian Gow <igow@hbs.edu>
2015-11-15 14:49:43 -0500

    Addressed curly apostrophes and "Ltd." cases.
    Resolves #3 and #11.

--- directorships/directorship_regex.py
+++ directorships/directorship_regex.py
@@ -31,7 +31,7 @@ def name_to_pattern(name):
     pattern = re.sub(r'\s+&\s+', "(?: & | and )", pattern)
 
     # Address curly apostrophes
-    pattern = re.sub(r"['\u2019]", "['\u2019]", pattern)
+    pattern = re.sub(u"’", "'", pattern)
 
     # US matches U.S. and vice versa
     pattern = re.sub(r'\bU\.?S\.?\b', 'U\.?S\.?', pattern)
@@ -41,7 +41,8 @@ def name_to_pattern(name):
     pattern = re.sub(r'\s+(?:CORP|ORATION)\b\.?', "(?: Co(?:rp(?:oration)?))?", pattern)
     pattern = re.sub(r'\s+HOLDINGS\b', "(?: Holdings)?", pattern)
     pattern = re.sub(r'\s+GROUP\b', "(?: Group)?", pattern)
-
+    pattern = re.sub(r'\s+LTD\b', "(?: Ltd)?", pattern)
+    
     # Allow spaces to be matched by hyphens
     pattern = re.sub(r'(?:\\-|\\ )+', "[-\\s]+", pattern)
 
@@ -50,7 +51,7 @@ def name_to_pattern(name):
     return pattern
 
 def apply_regex(bio, pattern):
-    """This function takes a bio and a regular expression patter and returns
+    """This function takes a bio and a regular expression pattern and returns
     the matches, if any.
     """
 
@@ -61,7 +62,10 @@ def apply_regex(bio, pattern):
         # Remove non-breaking spaces from names
         new_text = bio_text.replace(u'\xa0', u' ')
 
-        new_text = re.sub(r'\n', " ", bio_text)
+        # Convert curly apostrophes
+        new_text = re.sub(u"’", "'", new_text)
+
+        new_text = re.sub(r'\n', " ", new_text)
         new_text = re.sub(r'^\s*(The|THE)\s+', "", new_text)
         new_text = re.sub(r'\b-\s+\b', "-", new_text)
         new_text = re.sub(r'\s+', " ", new_text)
