fe1b41f99b2a72eab483b3913aee6ad17ac931a0
Ian Gow <igow@hbs.edu>
2017-03-24 15:06:36 -0400

    Updated code for creating a test sample for classification.

--- directorships/test_sample.R
+++ directorships/test_sample.R
@@ -1,74 +1,50 @@
 
 # Get a sample of observations to look at ----
-library(dplyr)
+suppressPackageStartupMessages(library(dplyr))
 pg <- src_postgres()
 
-sql <- sql("
-    SELECT a.director_id, a.fy_end, companies[1] AS company,
-        cik,
-        other_director_id, director, file_name,
-        other_directorships, other_start_date, other_end_date,
-        other_cik, directorid
-    FROM director_bio.bio_data AS a
-    INNER JOIN director_bio.other_directorships AS b
-    USING (director_id, fy_end)
-    INNER JOIN director_bio.regex_results AS d
-    USING (file_name, director_id, other_director_id)
-    WHERE non_match
-        AND date_filed > other_start_date  -- exclude future directorships
-        AND (other_first_date <= other_start_date -- always public only
-            AND other_last_date >= date_filed)")
+# We want to exclude observations that we've already looked at.
+test_sample <-
+    tbl(pg, sql("SELECT * FROM director_bio.test_sample"))
+
+other_directorships <-
+    tbl(pg, sql("SELECT * FROM director_bio.other_directorships"))
+
+bio_data <-
+    tbl(pg, sql("SELECT * FROM director_bio.bio_data"))
+
+regex_results <-
+    tbl(pg, sql("SELECT * FROM director_bio.regex_results"))
 
 tagging_url <- function(file_name) {
     temp <- gsub("^edgar/data/", "http://hal.marder.io/highlight/",
                  file_name)
     gsub("(\\d{10})-(\\d{2})-(\\d{6})\\.txt", "\\1\\2\\3", temp)
-
 }
 
-# We want to exclude observations that we've already looked at.
-already_sampled <-
-    tbl(pg, sql("
-        SELECT director_id, other_director_id, fy_end
-        FROM director_bio.test_data
-        UNION
-        SELECT director_id::equilar_director_id,
-            other_director_id::equilar_director_id, fy_end::date
-        FROM director_bio.test_data_2"))
-    # %>% select(director_id, other_director_id, fy_end)
-
-# Use CRSP to match tickers and CUSIPs to permcos ----
-full_sample <- tbl(pg, sql) %>%
-    anti_join(already_sampled) %>%
-    collect() %>%
+sample <-
+    other_directorships %>%
+    filter(date_filed > other_start_date) %>%     # exclude future directorships
+    filter(other_first_date <= other_start_date,  # always public only
+           other_last_date >= date_filed) %>%   # inner_join(bio_data) %>%
+    inner_join(
+        bio_data %>%
+            select(director_id, fy_end, director) %>%
+            distinct) %>%
+    inner_join(regex_results) %>%
+    filter(non_match) %>%
+    select(director_id, fy_end, cik, other_director_id, director,
+           file_name, other_directorships, other_start_date, other_end_date,
+           other_cik, directorid) %>%
+    anti_join(test_sample) %>%
+    mutate(rand = random()) %>%
+    arrange(rand) %>%
+    collect(n=50) %>%
+    select(-rand) %>%
     mutate(url = tagging_url(file_name))
 
-# Flag observations RAs are looking at already ----
-library(googlesheets)
-
-# You might need to run the next line first (remove # to do so).
-# gs_auth()
-gs <- gs_key("1PIEYkDnH7cgdelzcLipdw8H9p7Z5dbeg521POfLFhLw")
-to_retag <-  gs_read(gs, ws="to_retag") %>%
-    select(url) %>%
-    mutate(to_retag = TRUE)
-
-gs <- gs_key("1_8uFkS0jYS3j6oKCNlPYAhOnbd4d8gEE1nQ67yjoNGk")
-empty_director <-  gs_read(gs, ws="empty_director") %>%
-    select(url) %>%
-    mutate(empty_director = TRUE)
-
-# Take a random sample and save to Google Sheets ----
-sample <- full_sample %>%
-    left_join(to_retag) %>%
-    left_join(empty_director) %>%
-    sample_n(50)
-
-sample$director_id <- paste0("'", sample$director_id)
-sample$other_director_id <- paste0("'", sample$other_director_id)
-
-sample %>%
-    with(table(to_retag, empty_director, useNA="ifany"))
-
 library(readr)
-write_csv(sample, path = "~/Google Drive/director_bio/test_sample.csv")
+sample %>%
+    mutate(director_id = paste0("'", director_id)) %>%
+    mutate(other_director_id = paste0("'", other_director_id)) %>%
+    write_csv(path = "~/Google Drive/director_bio/test_sample.csv")
